2a936e360159d87c8ce456d52c621ae61537147c
Martin Wurst <martin.wurst@gmail.com>
2024-12-30 23:01:53 +0100

    geht unter http

--- DeckBuilderWASM/Program.cs
+++ DeckBuilderWASM/Program.cs
@@ -11,13 +11,16 @@ namespace DeckBuilderWASM
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            
             var httpClient = new HttpClient
             {
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
             };
-    //        var domain = builder.HostEnvironment.BaseAddress.Trim('/').Trim('\n').Replace("\n", "")           // Entfernt alle Zeilenumbrüche
-    //.Replace("\r", "");
-            httpClient.DefaultRequestHeaders.Add("Origin", builder.HostEnvironment.BaseAddress);
+
+            var domain = builder.HostEnvironment.BaseAddress.Trim('/').Trim('\n').Replace("\n", "")           // Entfernt alle Zeilenumbrüche
+    .Replace("\r", "");
+
+            httpClient.DefaultRequestHeaders.Add("Origin", domain);
 
             builder.Services.AddScoped(sp => httpClient);
             builder.Services.AddSingleton<Container>();
